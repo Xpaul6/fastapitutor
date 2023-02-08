55a43a4d86e2608033ba8df6bf9362593ef0f8b8
PLINTUS <Xpaul6@gmail.com>
2023-02-08 20:01:18 +0500

    models added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 dick = {
     1:'bruh',
@@ -6,6 +8,26 @@ dick = {
     3:'lmao',
     4:'cringe'
 }
+
+tools = {
+    1: {
+        'name': 'skrewdriver',
+        'size': 15,
+        'material': 'steel'
+    },
+
+    2: {
+        'name': 'drill',
+        'size': 20,
+        'material': 'aluminium'
+    }
+
+}
+
+class Tool(BaseModel):
+    name: str
+    size: int
+    material: str
 
 app = FastAPI()
 
@@ -23,3 +45,14 @@ async def get_memes(number_of_memes: int):
     for i in range(1, number_of_memes + 1):
         meme_list.append(dick[i])
     return meme_list
+
+@app.get('/get-tools-list')
+async def get_tools_list():
+    return tools
+
+@app.post('/add-tool/{tool_id}')
+async def add_tool(tool_id: int, tool: Tool):
+    if tool_id in tools:
+        return {'Error':'baka'}
+    tools[tool_id] = tool
+    return {'Success':''}
