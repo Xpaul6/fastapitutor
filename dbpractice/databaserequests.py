24a8ad47c4c7180a262c90ceea0cd2f1ae236b73
PLINTUS <Xpaul6@gmail.com>
2023-02-20 21:11:18 +0500

    123

--- dbpractice/databaserequests.py
+++ dbpractice/databaserequests.py
@@ -2,12 +2,10 @@ def get_students(connection):
     with connection.cursor() as cursor:
         getAllStudents = 'SELECT * FROM student'
         cursor.execute(getAllStudents)
-        rows = cursor.fetchall()
-        return rows
+        return cursor.fetchall()
 
 def get_student_by_id(connection, studentId):
     with connection.cursor() as cursor:
         getStudentById = f'SELECT * FROM student WHERE id={studentId}'
         cursor.execute(getStudentById)
-        row = cursor.fetchall()
-        return row
+        return cursor.fetchall()
