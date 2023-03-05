ba2ec1c8c39ffb0c82d3d94b18d0c8cc75cc776b
PLINTUS <Xpaul6@gmail.com>
2023-03-05 16:51:38 +0500

    123

--- dbpractice/databaserequests.py
+++ dbpractice/databaserequests.py
@@ -1,11 +1,15 @@
 def get_students(connection):
     with connection.cursor() as cursor:
-        getAllStudents = 'SELECT * FROM student'
-        cursor.execute(getAllStudents)
+        cursor.execute('SELECT * FROM student')
         return cursor.fetchall()
 
 def get_student_by_id(connection, studentId):
     with connection.cursor() as cursor:
-        getStudentById = f'SELECT * FROM student WHERE id={studentId}'
-        cursor.execute(getStudentById)
+        cursor.execute(f'SELECT * FROM student WHERE id={studentId}')
         return cursor.fetchall()
+
+def create_student(connection, new_student):
+    with connection.cursor() as cursor:
+        cursor.execute(f'INSERT INTO student (name, faculty) VALUES ("{new_student.name}","{new_student.faculty}")')
+        connection.commit()
+        return {'Successfully':'added'}
--- dbpractice/mainapp.py
+++ dbpractice/mainapp.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
-from config import host, user, password, db_name, connection
+from pydantic import BaseModel
+from config import connection
 import databaserequests
+
+class Student(BaseModel):
+    name: str
+    faculty: str
 
 
 app = FastAPI()
@@ -12,3 +17,7 @@ async def get_students():
 @app.get('/get-student-by-id/{studentId}')
 async def get_student_by_id(studentId: int):
     return databaserequests.get_student_by_id(connection, studentId)
+
+@app.post('/create-student')
+async def create_student(new_student: Student):
+    return databaserequests.create_student(connection, new_student)
